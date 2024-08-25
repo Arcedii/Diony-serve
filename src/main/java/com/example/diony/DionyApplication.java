21b3253c5819afd917990ceb0295a95e4c97856b
Arcedii <91275440+Arcedii@users.noreply.github.com>
2024-08-25 09:58:25 +0300

    initial-commit

---
+++ src/main/java/com/example/diony/DionyApplication.java
@@ -0,0 +1,13 @@
+package com.example.diony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionyApplication.class, args);
+	}
+
+}
